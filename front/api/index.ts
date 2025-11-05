787f5e4444b6d629506c7592100fa7b9e7d6dff6
bassy1992 <wyarquah@gmail.com>
2025-11-05 09:32:04 +0000

    Remove all Vercel configuration and dependencies

--- front/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "../server/index";
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-const app = createServer();
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
