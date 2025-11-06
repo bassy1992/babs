6c9dce1700c1c8435f1afe1e490eb8ac0ab43049
bassy1992 <wyarquah@gmail.com>
2025-11-06 16:18:43 +0000

    Add app-root to nixpacks.toml and simplify start.sh for Railway deployment

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -e
 cd back
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python -m gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
