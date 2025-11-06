fc73de43d55f363ff56524990f7ef11f1de80070
bassy1992 <wyarquah@gmail.com>
2025-11-06 16:12:00 +0000

    Fix start.sh to use python3 and proper gunicorn invocation

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
+
 cd back
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+python3 -m gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
